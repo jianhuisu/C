e398f6dbae8670abb2a6ec945c5605042d234e2a
sujianhui <1051034413@qq.com>
2020-03-05 23:42:02 +0800

    DFS

--- string/multiple_way_define_string.c
+++ string/multiple_way_define_string.c
@@ -5,18 +5,24 @@
 
 int main(int argc, char * argv[])
 {
+    // method.1 correct
     char name1[20] = "ddddddddddddddd";
 
+    // method.2 error
     char name2[20];
     *name2 = "sssssssssssssss";
 
+    // method.3 correct
     char * name3;
     name3 = "xxxxxxxxxxxxxxxx";
+
+    // method.4 correct
+    char name4[20]={'a', 'b', 'c','\n'};
 
     printf(" %s \n",name1);
     printf(" %s \n",name2);
     printf(" %s \n",name3);
-
+    printf(" %s \n",name4);
 
     return 0;
 
