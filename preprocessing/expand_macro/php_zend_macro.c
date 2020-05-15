c9844c0a074c6e22cee2ef49315554a46472d9fa
sujianhui <1051034413@qq.com>
2020-05-15 15:39:38 +0800

    expand macro

---
+++ preprocessing/expand_macro/php_zend_macro.c
@@ -0,0 +1,40 @@
+# define CG(v) (compiler_globals.v)
+# define EG(v) (executor_globals.v)
+# define PG(v) (core_globals.v)
+# define SG(v) (sapi_globals.v)
+
+
+
+int main(void)
+{
+    if (PG(internal_encoding) && PG(internal_encoding)[0]) {
+        return PG(internal_encoding);
+    } else if (SG(default_charset)) {
+        return SG(default_charset);
+    }
+}
+
+/*
+
+sujianhui@dev529> cpp php_zend_macro.c
+# 1 "php_zend_macro.c"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 1 "<command-line>" 2
+# 1 "php_zend_macro.c"
+
+
+
+
+
+int main(void)
+{
+    if ((core_globals.internal_encoding) && (core_globals.internal_encoding)[0]) {
+        return (core_globals.internal_encoding);
+    } else if ((sapi_globals.default_charset)) {
+        return (sapi_globals.default_charset);
+    }
+}
+
+*/
---
+++ shell/demo/ghome_bashrd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z $TERMINAL_EMULATOR ]
+then
+# ghome terminal
+
+export PS1="\[\e[36;40m\][\u@\[\e[32;40m\]\h \W]$>\[\e[0m\]"
+
+else
+
+# jetbrains idea terminal
+# echo "welcome to jetbrains terminal"
+export PS1="\[\e[36;1m\]\u@\[\e[32;1m\]\H> \[\e[0m\]"
+fi
