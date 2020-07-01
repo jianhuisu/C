3b099e25487759f9f183603fb6744da4213feecf
sujianhui <1051034413@qq.com>
2020-07-01 19:07:41 +0800

    heap sort

--- a_test/test.c
+++ a_test/test.c
@@ -2,6 +2,9 @@
 #define MAX_SIZE 10
 int wait_sort[MAX_SIZE] = {11,15,20,13,17,65,27,49,99,18};
 
+//#define MAX_SIZE 3
+//int wait_sort[MAX_SIZE] = {15,11,20};
+
 void show(int * s,int length)
 {
     int i ;
@@ -14,47 +17,55 @@ void show(int * s,int length)
 
 void swap(int * i,int * j)
 {
-        int temp;
-        temp = *i;
-        *i = *j;
-        *j = temp;
+    int temp;
+    temp = *i;
+    *i = *j;
+    *j = temp;
 }
 
-void quick_sort(int a[],int left,int right)
+void build_min_heap(int a[],int cur_node_index,int len)
 {
-    // 快速错误
-    if(left >= right ){
-        return ;
+    int child;
+    // 保障端有序
+    while(cur_node_index < len){
+        child = cur_node_index * 2 + 1;
+
+        // 较小者上升
+        if(child < len && a[child] > a[child + 1]){
+            child++;
+        }
+
+        // 三个数中只需要确定最小值即可,根 左 右 三个结点 ，需要两次相互比较即可以得出，如果需要确定三个数的大小顺序，则需要比较三次
+        if(child < len && a[child] < a[cur_node_index]){
+            swap(&a[child],&a[cur_node_index]);
+            cur_node_index = child;
+        } else {
+            break;
+        }
     }
 
-    int i = left;
-    int j = right;
-    int pivot = a[left];
 
-    while(left < right)
-    {
-        // 从右边找小值
-        while(left < right && pivot < a[right]){
-            right--;
-        }
-        swap(&a[left],&a[right]);
+}
 
-        // 从左边找大值
-        while(left < right && pivot > a[left]){
-            left++;
-        }
-        swap(&a[left],&a[right]);
+void heap_sort(int a[],int len)
+{
+    // 按降序排列 将无序序列构建为小顶堆
+    int i;
+    for( i = len/2 - 1;i >= 0; i--){
+        build_min_heap(a,i,len - 1);
     }
 
-    // 递归调用
-    quick_sort(a,i,left - 1);
-    quick_sort(a,left + 1,j);
+    // 交换 根结点 与 叶子结点
+    for(i = len - 1;i>= 0;i--){
+        swap(&a[0],&a[i]);
+        build_min_heap(a,0,i - 1);
+    }
 
 }
 
 int main(void)
 {
-    quick_sort(wait_sort,0,MAX_SIZE - 1);
+    heap_sort(wait_sort,MAX_SIZE);
     show(wait_sort,MAX_SIZE);
     return 0;
 }
