5c80f03b4227ab2322ae6cf59afbc24cd9514ab8
sujianhui <1051034413@qq.com>
2020-07-07 21:35:43 +0800

    ..

--- a_test/test.c
+++ a_test/test.c
@@ -2,22 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-int global = 0;       // 全局初始化区域
-char * p1;            // 全局未初始化区域
-
 int main(void)
 {
-    int a;               // 栈空间
-    char s[] = "abcdefg";// 栈空间
-    char * p2;           // 栈空间
+    int a = 1;
+    int * pi = &a;
+    int ** pii = &pi;
 
-    char *p3 = "1234567890";  // "1234567890" 在常量区 p3在栈上
-    static int c = 0;         // 全局静态初始化区
+    printf(" %p %d \n",pi,*pi);
+    printf(" %p %p\n",pii,*pii);
 
-    p1 = (char * )malloc(100 * sizeof(char));  // 申请的100字节 属于堆空间
-    p2 = (char *)malloc(200 * sizeof(char));   // 申请 200字节属于堆空间
-
-    strcpy(p1,"123456789");
-
+    int b = 2;
+    *pi = b;
+    // *指针， 变量存储的常规变量的存储地址.
+    // ** 指向指针的指针,就是二级指针，它第一级指针存储的的内容还是指针，该指针指向
     return 0;
 }
