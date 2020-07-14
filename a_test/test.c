8445735ae246166ff65bea98783d48e7c78e6f6a
sujianhui <1051034413@qq.com>
2020-07-14 15:37:12 +0800

    complete

--- a_test/test.c
+++ a_test/test.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
 
-int main(void)
-{
-    char name[4][5] = {
-            {'a','b','c','d','e'},
-            {'a','b','c','d','e'},
-            {'a','b','c','d','e'},
-            {'a','b','f','d','e'},
-    };
 
-    int i;
-    for(i = 0;i< 4 * 5;i++)
-    {
-        printf("name[%d][%d] => %c ",i/5,i%5,name[i/5][i%5]);
-        if( (i + 1) % 5  == 0 ){
-            printf("\n");
+static int binary_search_use_rec(int findData,int * a,int start,int end)
+{
+    if(start < end){
+        int mid = (start + end) / 2;
+        if( findData == a[mid] ){
+            //return a[mid];
+            return mid;
+        } else if(findData < a[mid]){
+            return binary_search_use_rec(findData,a,start,mid - 1);
+        } else {
+            return binary_search_use_rec(findData,a,mid + 1,end);
         }
     }
 
+    return -1;
+}
+
+static int binary_search_without_rec(int findData,int * a,int start,int end)
+{
+    while(start < end){
+        int mid = (start + end) / 2;
+        if( findData == a[mid] ){
+            //return a[mid];
+            return mid;
+        } else if(findData < a[mid]){
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
+int main(void)
+{
+    int findData = 3;
+    int a[10] = {1,2,3,4,5,6,7,8,9,10};
+
+    printf("index : %d \n",binary_search_without_rec(findData,a,0,9));
     return 0;
 }
